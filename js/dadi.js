cadbff85044e2462363119b1736ecf6e46bd5427
PasqualeDiMatteo <pasqualedm94@gmail.com>
2023-05-31 17:30:56 +0200

    (BONUS) Added Bonus Dice

--- js/dadi.js
+++ js/dadi.js
@@ -18,20 +18,30 @@ console.log("JSOK");
    - Visualizzare un messaggio "pareggio"
 */
 
-// I generate two random numbers
-const userNumber = Math.floor(Math.random() * 6) + 1;
-const cpuNumber = Math.floor(Math.random() * 6) + 1;
+const plays = document.getElementById("plays");
+const winnerElement = document.getElementById("winner");
+const cpuElement = document.getElementById("cpu");
+const userElement = document.getElementById("user");
 
-console.log(userNumber, cpuNumber);
+plays.addEventListener("click", function () {
+  // I generate two random numbers
 
-let result = "C'è stato un pareggio";
+  const userNumber = Math.floor(Math.random() * 6) + 1;
+  const cpuNumber = Math.floor(Math.random() * 6) + 1;
 
-// Conditions
+  console.log(userNumber, cpuNumber);
 
-if (userNumber > cpuNumber) {
-  result = "Ha vinto l'utente";
-} else if (userNumber < cpuNumber) {
-  result = "Ha vinto la CPU";
-}
+  let result = "C'è stato un pareggio";
 
-console.log(result);
+  // Conditions
+
+  if (userNumber > cpuNumber) {
+    result = "Ha vinto l'utente";
+  } else if (userNumber < cpuNumber) {
+    result = "Ha vinto la CPU";
+  }
+
+  winnerElement.innerText = result;
+  cpuElement.innerText = `Dado  Cpu ${cpuNumber}`;
+  userElement.innerText = `Dado utente ${userNumber}`;
+});
